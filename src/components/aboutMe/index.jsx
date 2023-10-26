627ad6c47d24adc34cf969911fc2941f75de0b4e
Rajendran Giabilan <giabilan.r@gmail.com>
2023-10-26 12:36:12 +0200

    updated of AboutMe text

--- src/components/aboutMe/index.jsx
+++ src/components/aboutMe/index.jsx
@@ -11,14 +11,14 @@ const AboutMe = () => {
           web Front end. Je suis passionné par la technologie et j&apos;aime
           créer des choses qui ont un impact positif sur le monde. J&apos;ai
           commencé à apprendre à l&apos;âge de 20 ans, sans aucune connaissance
-          préalable. J&apos;ai appris en autodidacte, en suivant des tutoriels
-          en ligne et en travaillant sur des projets d&apos; OpenClassrooms. En
-          9 mois, j&apos;ai acquis les compétences nécessaires pour développer
-          des sites web. J&apos;ai également appris à travailler en équipe et à
-          gérer des projets. Je suis toujours en train d&apos;apprendre et de
-          m&apos;améliorer, mais je suis fier de ce que j&apos;ai accompli
-          jusqu&apos;à présent. Je suis impatient de voir ce que l&apos;avenir
-          me réserve.
+          préalable. J&apos;ai appris en suivant des tutoriels et en particulier grâce à la
+          formation d&apos;Intégrateur Web de chez OpenClassrooms et le tout en
+          9 mois seulement, j&apos;ai acquis les compétences nécessaires pour
+          développer des sites web. J&apos;ai également appris à travailler en
+          équipe et à gérer des projets. Je suis toujours en train
+          d&apos;apprendre et de m&apos;améliorer, mais je suis fier de ce que
+          j&apos;ai accompli jusqu&apos;à présent. Je suis impatient de voir ce
+          que l&apos;avenir me réserve.
         </p>
       </div>
     </div>
